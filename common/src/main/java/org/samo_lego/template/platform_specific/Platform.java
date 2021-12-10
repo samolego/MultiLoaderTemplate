4645610a234199c288103182a380ba66309fdd96
samolego <34912839+samolego@users.noreply.github.com>
2021-12-10 09:21:02 +0100

    Common mixins working in forge

--- common/src/main/java/org/samo_lego/template/platform_specific/Platform.java
+++ common/src/main/java/org/samo_lego/template/platform_specific/Platform.java
@@ -5,6 +5,25 @@ import net.minecraft.server.level.ServerPlayer;
 import java.nio.file.Path;
 
 public abstract class Platform {
+
+    private final Environment environment;
+
+    public Platform() {
+        this(Environment.UNKNOWN);
+    }
+
+    public Platform(Environment environment) {
+        this.environment = environment;
+    }
+
+    /**
+     * Gets mod loader.
+     * @return mod loader type.
+     */
+    public Environment getEnvironment() {
+        return this.environment;
+    }
+
     /**
      * Gets config directory path.
      * @return config directory path.
@@ -18,4 +37,10 @@ public abstract class Platform {
      * @return true if player has permission, false otherwise.
      */
     public abstract boolean hasPermission(ServerPlayer player, String permission);
+
+    public enum Environment {
+        FABRIC,
+        FORGE,
+        UNKNOWN
+    }
 }
--- forge/src/main/java/org/samo_lego/template/forge/mixin/SampleMixin.java
+++ forge/src/main/java/org/samo_lego/template/forge/mixin/SampleMixin.java
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class SampleMixin {
     @Inject(at = @At("HEAD"), method = "init()V")
     private void init(CallbackInfo info) {
-        Template.LOGGER.info("This line is printed by an example mod mixin in fabric package!");
+        Template.LOGGER.info("This line is printed by an example mod mixin in forge package!");
     }
 }
